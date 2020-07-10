c86197fa63924a0f24eb3243e849724ba35382bb
Kevin Tierney <kevin.tierney@uni-bielefeld.de>
2020-07-10 10:01:06 +0200

    Adding griewank function

--- problem_func.py
+++ problem_func.py
@@ -6,7 +6,7 @@ import sys
 from problem import Problem
 
 def add_parser_args(parser):
-    parser.add_argument('--func', choices=['rastrigin'], default='rastrigin', help='Unconstrained, continuous function to solve')
+    parser.add_argument('--func', choices=['rastrigin','griewank'], default='rastrigin', help='Unconstrained, continuous function to solve')
     parser.add_argument('--dims', type=int, default=10, help='Number of dimensions')
 
 def read_instance(inst_path):
@@ -23,5 +23,10 @@ class func(Problem):
         if self.func == 'rastrigin':
             tmp = np.power(keys, 2) - np.cos(keys * 2 * np.pi)
             return np.sum(tmp, axis=1) + (10 * self.dimensions)
+        elif self.func == 'griewank':
+            right_term = np.prod(np.cos(keys / np.sqrt(np.arange(1, self.dimensions + 1))), axis=1)
+            mid_term = np.sum(np.power(keys, 2) / 4000.0, axis=1)
+            return mid_term + right_term + 1.0
+
 
 
