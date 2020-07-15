d45a57baa1e73a6f10a6beffdbb89aad0e88d0c1
Kevin Tierney <kevin.tierney@uni-bielefeld.de>
2020-07-15 17:08:26 +0200

    adding ackley func

--- problem_func.py
+++ problem_func.py
@@ -6,7 +6,7 @@ import sys
 from problem import Problem
 
 def add_parser_args(parser):
-    parser.add_argument('--func', choices=['rastrigin','griewank'], default='rastrigin', help='Unconstrained, continuous function to solve')
+    parser.add_argument('--func', choices=['rastrigin','griewank','ackley'], default='rastrigin', help='Unconstrained, continuous function to solve')
     parser.add_argument('--dims', type=int, default=10, help='Number of dimensions')
 
 def read_instance(inst_path):
@@ -29,6 +29,16 @@ class func(Problem):
             right_term = np.prod(np.cos(keys / np.sqrt(np.arange(1, self.dimensions + 1))), axis=1)
             mid_term = np.sum(np.power(keys, 2) / 4000.0, axis=1)
             return mid_term + right_term + 1.0
+        elif self.func == 'ackley':
+            aa = 20
+            bb = 0.2
+            cc = 2 * np.pi
+
+            onedivn = 1.0/len(keys)
+            exp_l = (-1.0 * aa) * np.exp((-1.0 * bb) * np.sqrt(onedivn * np.sum(np.power(keys, 2), axis=1)))
+            exp_r = np.exp(onedivn * np.sum(np.cos(cc * keys), axis=1))
+            return exp_l - exp_r + aa + np.exp(1)
+
 
 
 
