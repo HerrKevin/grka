fb48d816e4fbd4ba469483c679c663771d30e2c2
Kevin Tierney <kevin.tierney@uni-bielefeld.de>
2020-07-16 09:47:50 +0200

    adding some more functions

--- problem_func.py
+++ problem_func.py
@@ -6,7 +6,7 @@ import sys
 from problem import Problem
 
 def add_parser_args(parser):
-    parser.add_argument('--func', choices=['rastrigin','griewank','ackley'], default='rastrigin', help='Unconstrained, continuous function to solve')
+    parser.add_argument('--func', choices=['ackley', 'alpine','griewank','periodic','rastrigin','salomon'], default='rastrigin', help='Unconstrained, continuous function to solve')
     parser.add_argument('--dims', type=int, default=10, help='Number of dimensions')
 
 def read_instance(inst_path):
@@ -30,7 +30,7 @@ class func(Problem):
             mid_term = np.sum(np.power(keys, 2) / 4000.0, axis=1)
             return mid_term + right_term + 1.0
         elif self.func == 'ackley':
-            aa = 20
+            aa = 20 # TODO parameterize?
             bb = 0.2
             cc = 2 * np.pi
 
@@ -38,6 +38,14 @@ class func(Problem):
             exp_l = (-1.0 * aa) * np.exp((-1.0 * bb) * np.sqrt(onedivn * np.sum(np.power(keys, 2), axis=1)))
             exp_r = np.exp(onedivn * np.sum(np.cos(cc * keys), axis=1))
             return exp_l - exp_r + aa + np.exp(1)
+        elif self.func == 'alpine':
+            return np.sum(np.abs((keys * np.sin(keys)) + (0.1 * keys)), axis=1)
+        elif self.func == 'periodic':
+            # TODO I don't think this one is right
+            return 1.0 + np.sum(np.power(np.sin(keys), 2), axis=1) - 0.1 * np.exp(-1.0 * np.sum(np.power(keys, 2), axis=1))
+        elif self.func == 'salomon':
+            sqk = np.sum(np.power(keys, 2), axis=1)
+            return 1.0 - np.cos(2.0 * np.pi * np.sqrt(sqk)) + 0.1 * np.sqrt(sqk)
 
 
 
