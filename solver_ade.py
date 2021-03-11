458534552e5ef3b3e53401b10df1fc3933bd4343
Kevin Tierney <kevin.tierney@uni-bielefeld.de>
2021-03-11 11:48:16 +0100

    adding ade parameters

--- solver_ade.py
+++ solver_ade.py
@@ -17,12 +17,17 @@ def add_parser_args(parser):
 
     parser.add_argument('--ade_alg', choices=['de','ilshade','jade','jso','lshade','lshadecnepsin','mpede','sade','saepsdemmts','shade'], default='ilshade', help='Choice of DE algorithm from pyade')
     parser.add_argument('--pop_size', type=int, action=min_max_arg('Population', 50), default=500, help='DE population size')
-#     parser.add_argument('--CR', type=float, action=min_max_arg('CR', 0, 1), default=0.3, help='DE parameter CR')
-#     parser.add_argument('--F', type=float, action=min_max_arg('F', 0, 2), default=0.8, help='DE parameter F')
-#     parser.add_argument('--no_batch_mode', action='store_true', default=False, help='Batch mode performs fitness evaluations in batches instead of sequentially. When this is enabled, batch mode is disabled and things will be slower. Note that batchmode does not use multiple threads; it just vectorizes DE, but that slightly changes the way the algorithm works.')
-#     parser.add_argument('--convergence_eps', type=float, default=1e-4, help='Stop if the average change in objective function over the last convergence_last generations falls below this epsilon')
-#     parser.add_argument('--convergence_last', type=float, default=5, help='Stop if the average change in objective function over the last n generations falls below convergence_eps')
-#     parser.add_argument('--convergence_imp', action='store_true', default=False, help='Stop if the average change in objective function over the last convergence_last generations falls below convergence_eps')
+    parser.add_argument('--shade_memory_size', type=int, action=min_max_arg('SHADE Mem size', 2), default=6, help='((i)l)shade(cnepsin)/jso memory size')
+    parser.add_argument('--de_f', type=float, action=min_max_arg('DE_f', 0.0, 2.0), default=0.5, help='DE f')
+    parser.add_argument('--de_cr', type=float, action=min_max_arg('DE_cr', 0.0, 1.0), default=0.9, help='DE f')
+    parser.add_argument('--de_cross', choices=['bin','exp'], default=bin, help='DE cross')
+    parser.add_argument('--jade_p', type=float, action=min_max_arg('JADE_p', 0.0, 1.0), default=0.0, help='JADE/MPEDE p; note: if set less than 1e-3 the parameter will be set to max(0.05, 3.0/pop_size); default value for mpede suggested to be 0.04')
+    parser.add_argument('--jade_c', type=float, action=min_max_arg('JADE_c', 0.0, 1.0), default=0.1, help='JADE/MPEDE c')
+    parser.add_argument('--mpede_ng', type=int, action=min_max_arg('MPEDE_ng', 1, 200), default=20, help='MPEDE ng')
+    parser.add_argument('--mpede_lambda1', type=float, action=min_max_arg('MPEDE_l1', 0.0, 1.0), default=0.2, help='MPEDE lambda1 (note: will be made into prob dist)')
+    parser.add_argument('--mpede_lambda2', type=float, action=min_max_arg('MPEDE_l2', 0.0, 1.0), default=0.2, help='MPEDE lambda2 (note: will be made into prob dist)')
+    parser.add_argument('--mpede_lambda3', type=float, action=min_max_arg('MPEDE_l3', 0.0, 1.0), default=0.2, help='MPEDE lambda3 (note: will be made into prob dist)')
+    parser.add_argument('--mpede_lambda4', type=float, action=min_max_arg('MPEDE_l4', 0.0, 1.0), default=0.4, help='MPEDE lambda4 (note: will be made into prob dist)')
 
 
 class ade(Solver):
@@ -38,13 +43,33 @@ class ade(Solver):
         return super().terminate()
 
     def solve(self):
-        solver_ = il.import_module(f'pyade.{self.args.ade_alg}')
+        alg = self.args.ade_alg
+        solver_ = il.import_module(f'pyade.{alg}')
         params = solver_.get_default_params(dim=self.problem.dimensions)
         params['population_size'] = self.args.pop_size
         params['max_evals'] = self.args.max_evals
         params['func'] = lambda xx: (self.problem.batch_evaluate(np.array([xx])))[0]
 #         params['func'] = lambda xx: np.sum(np.power(xx, 2) - np.cos(xx * 2 * np.pi)) + (10 * self.problem.dimensions)
         params['bounds'] = np.array([[0,1]] * self.problem.dimensions)
+
+        if alg in ['ilshade', 'lshade', 'shade', 'jso', 'lshadecnepsin']:
+            params['memory_size'] = self.args.shade_memory_size
+        elif alg == 'de':
+            params['f'] = self.args.de_f
+            params['cr'] = self.args.de_cr
+            params['cross'] = self.args.cross
+        elif alg in ['jade', 'mpede']:
+            if self.args.jade_p < 1e-3:
+                params['p'] = max(0.05, 3.0 / self.pop_size)
+            else:
+                params['p'] = self.args.jade_p
+            params['c'] = self.args.jade_c
+            if alg == 'mpede':
+                params['ng'] = self.args.mpede_ng
+                lambdas = np.array([self.args.lambda1, self.args.lambda2, self.args.lambda3, self.args.lambda4])
+                lambdas = lambdas / np.sum(lambdas)
+                params['lambdas'] = lambdas
+
 
         self.best_fit = 1e99
 
