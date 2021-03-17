429cefa86208374ccdacbfd61f6fc5ac28618a84
Kevin Tierney <kevin.tierney@upb.de>
2021-03-17 17:43:04 +0100

    removing smp=f for future tests

--- scripts/ccs_submit_func_test.sh
+++ scripts/ccs_submit_func_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#CCS --res="rset=1:ncpus=1:mem=4g:smp=f+4:ncpus=16:mem=60g:smp=f"
-#CCS -t 4d
+#CCS --res="rset=1:ncpus=1:mem=4g:smp=f+4:ncpus=16:mem=60g"
+#CCS -t 1h
 #CCS --join oe
-#CCS --output /upb/departments/pc2/users/k/kbt/research/bbochallenge/cluster_junk/tuninglog.%reqid.out
-#CCS -N l2gpydgga
+#CCS --output /upb/departments/pc2/users/k/kbt/research/grka/tuning/cluster_junk/tuninglog.%reqid.out
+#CCS -N gtune_FUNC
+
+# NOTE: :smp=f if doing runtime tuning
 
 DGGA_NODES=4 # NOTE update above before the second ncpus! Note: population is determined automatically in the run script
 rnum=$(shuf -i 100-999 -n 1)
@@ -14,7 +16,7 @@ TUNE_DIR="/scratch/hpc-prf-winf4/kbt/research/grka/"
 SCENARIO="/scratch/hpc-prf-winf4/kbt/research/grka/tuning/scenario_bbo/"
 WORKER_DIR="/scratch/hpc-prf-winf4/kbt/research/grka/tuning/worker_output/"
 
-OUT_FILE=${TUNE_DIR}/output/bbo_l2_${CCS_REQID}.out
+OUT_FILE=${TUNE_DIR}/output/grka_func_10k_${CCS_REQID}.out
 
 cd ${TUNE_DIR}
 
