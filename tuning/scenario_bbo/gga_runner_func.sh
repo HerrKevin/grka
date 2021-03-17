700fe89c96645ecb9d8d11741640801663487344
Kevin Tierney <kevin.tierney@upb.de>
2021-03-17 17:27:28 +0100

    runner changes to func scenario

--- tuning/scenario_bbo/gga_runner_func.sh
+++ tuning/scenario_bbo/gga_runner_func.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd /Users/kbt/Research/grka/
+cd /scratch/hpc-prf-winf4/kbt/research/grka/
+source grkaenv/bin/activate
 
 inst=$1
 
@@ -9,6 +10,7 @@ IFS='_' read -ra sp <<< "${inst}"
 func=${sp[0]}
 finst=${sp[1]}
 seed=${sp[2]}
+
 
 python3 grka.py func /dev/null --func ${func} --finst ${finst} --seed ${seed} --tuner --use_gap --max_evals 10000 "${@:4}"
 
