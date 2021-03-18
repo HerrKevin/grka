04b8ef01db4be993018279ad7b9ba84d4f87e25e
Kevin Tierney <kevin.tierney@upb.de>
2021-03-18 13:00:35 +0100

    setting scenario dims to 20

---
+++ tuning/scenario_bbo_20/gga_runner_func.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /scratch/hpc-prf-winf4/kbt/research/grka/
+source grkaenv/bin/activate
+
+inst=$1
+
+IFS='_' read -ra sp <<< "${inst}"
+
+func=${sp[0]}
+finst=${sp[1]}
+seed=${sp[2]}
+
+
+python3 grka.py func /dev/null --func ${func} --finst ${finst} --dims 20 --seed ${seed} --tuner --use_gap --max_evals 10000 "${@:4}"
+
