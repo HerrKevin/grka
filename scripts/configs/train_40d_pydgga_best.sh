5f8ba6687617b5d80f9bc70879d525dad407dac8
Kevin Tierney <kevin.tierney@upb.de>
2021-03-24 09:57:51 +0100

    adjusting benchmark config location

---
+++ scripts/configs/train_40d_pydgga_best.sh
@@ -0,0 +1,4 @@
+inst_file=tuning/scenario_bbo_40/instances.txt
+job_name=train_40d_pydgga_best
+dims=40
+params="--CMA_active False --AdaptSigma False --CMA_cmean 0.37743319097486533 --CSA_damp_mueff_exp 0.11523239150956821 --CSA_dampfac 14 --CSA_disregard_length False --mean_shift_line_samples True --CMA_on 5 --CMA_rankmu 0.6691602851094957 --CMA_rankone 0.6634785863158259 --CSA_squared True --solver cmaes"
