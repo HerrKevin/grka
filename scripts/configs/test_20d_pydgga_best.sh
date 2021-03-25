0e753d32466edd83b052c48800427f324357fc01
Kevin Tierney <kevin.tierney@upb.de>
2021-03-25 09:16:58 +0100

    pydgga func update

--- scripts/configs/test_20d_pydgga_best.sh
+++ scripts/configs/test_20d_pydgga_best.sh
@@ -1,4 +1,5 @@
 inst_file=tuning/scenario_bbo_20/instances_test.txt
 job_name=test_pydgga_best
 dims=20
+max_evals=10000
 params="--solver cmaes --CMA_active False --AdaptSigma True --CMA_cmean 0.2786285866191009 --CSA_damp_mueff_exp 0.3051143287224861 --CSA_dampfac 18 --CSA_disregard_length False --mean_shift_line_samples False --CMA_on 2 --CMA_rankmu 1.3065333931972198 --CMA_rankone 1.6522252431859372 --CSA_squared True"
--- scripts/configs/test_40d_pydgga_best.sh
+++ scripts/configs/test_40d_pydgga_best.sh
@@ -1,4 +1,5 @@
 inst_file=tuning/scenario_bbo_40/instances_test.txt
 job_name=test_40d_pydgga_best
 dims=40
+max_evals=50000
 params="--CMA_active False --AdaptSigma False --CMA_cmean 0.37743319097486533 --CSA_damp_mueff_exp 0.11523239150956821 --CSA_dampfac 14 --CSA_disregard_length False --mean_shift_line_samples True --CMA_on 5 --CMA_rankmu 0.6691602851094957 --CMA_rankone 0.6634785863158259 --CSA_squared True --solver cmaes"
