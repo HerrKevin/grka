f7dec28917c9ee88dbf2e207fdafb3f3033a7d3e
Kevin Tierney <kevin.tierney@upb.de>
2021-03-30 14:56:12 +0200

    j30 mod configs

---
+++ scripts/configs/test_rcpsp_j30_mod_ilshade_default.sh
@@ -0,0 +1,7 @@
+inst_file=tuning/scenario_rcpsp_j30/instances_test.txt
+job_name=test_rcpsp_j30_mod_ilshade_default
+max_evals=25000
+max_cpu=120
+params2="--mod_makespan_l 1"
+out_dir=j30_mod
+params="--solver ade --ade_alg ilshade"
---
+++ scripts/configs/train_rcpsp_j30_mod_lshade_default.sh
@@ -0,0 +1,7 @@
+inst_file=tuning/scenario_rcpsp_j30/instances.txt
+job_name=train_rcpsp_j30_mod_lshade_default
+max_evals=25000
+max_cpu=120
+params2="--mod_makespan_l 1"
+out_dir=j30_mod
+params="--solver ade --ade_alg lshade"
