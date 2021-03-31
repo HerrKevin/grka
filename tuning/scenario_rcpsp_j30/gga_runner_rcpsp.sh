e6543f67b1a1ffab32345a8f3b63913441219de0
Kevin Tierney <kevin.tierney@upb.de>
2021-03-31 09:11:20 +0200

    adding truncate_final option to RCPSP gga runner

--- tuning/scenario_rcpsp_j30/gga_runner_rcpsp.sh
+++ tuning/scenario_rcpsp_j30/gga_runner_rcpsp.sh
@@ -7,5 +7,5 @@ inst=$1
 seed=$3
 
 
-python3 grka.py rcpsp ${inst} --seed ${seed} --max_cpu 120 --tuner --max_evals 25000 "${@:4}"
+python3 grka.py rcpsp ${inst} --seed ${seed} --max_cpu 120 --tuner --max_evals 25000 --truncate_final "${@:4}"
 
